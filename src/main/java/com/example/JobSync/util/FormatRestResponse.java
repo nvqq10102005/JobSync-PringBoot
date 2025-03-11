af4d35d13aaf01fe4398c033d1676acc4929ff54
Nguyen Van Quy <142620727+nvqq10102005@users.noreply.github.com>
2025-03-11 22:32:00 +0700

    yeh

--- src/main/java/com/example/JobSync/util/FormatRestResponse.java
+++ src/main/java/com/example/JobSync/util/FormatRestResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:25e4363c7c68f6be3d243420f065381c6d96bf73c5d4de9ee4583521b9ce12ae
-size 71
+oid sha256:0759ca51dd2f6ba0bd671e30db9c41b0a530d963eab8cf931fcb487119586c09
+size 847
